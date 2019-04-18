0c6c5979ee22b0e088876decfe28fe7de9da145a
Mahen Gandhi <mahen.gandhi@civilmachines.com>
2019-04-19 00:56:02 +0530

    Added

---
+++ lib/skeleton_text.dart
@@ -0,0 +1,78 @@
+library skeleton_text;
+
+import 'package:flutter/material.dart';
+
+class SkeletonAnimation extends StatefulWidget {
+  final Widget child;
+  final Color shimmerColor;
+  final Color gradientColor;
+  final Curve curve;
+
+  SkeletonAnimation(
+      {@required this.child,
+      this.shimmerColor = Colors.white54,
+      this.gradientColor = const Color.fromARGB(0, 244, 244, 244),
+      this.curve = Curves.fastOutSlowIn,
+      Key key})
+      : super(key: key);
+
+  @override
+  _SkeletonAnimationState createState() => _SkeletonAnimationState();
+}
+
+class _SkeletonAnimationState extends State<SkeletonAnimation>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    );
+
+    _controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        widget.child,
+        Positioned.fill(
+            child: ClipRect(
+          child: AnimatedBuilder(
+            animation: _controller,
+            builder: (context, child) {
+              return FractionallySizedBox(
+                widthFactor: .2,
+                alignment: AlignmentGeometryTween(
+                  begin: Alignment(-1.0 - .2 * 3, .0),
+                  end: Alignment(1.0 + .2 * 3, .0),
+                ).chain(CurveTween(curve: widget.curve)).evaluate(_controller),
+                child: child,
+              );
+            },
+            child: DecoratedBox(
+                decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  widget.gradientColor,
+                  widget.shimmerColor,
+                ],
+              ),
+            )),
+          ),
+        ))
+      ],
+    );
+  }
+}
---
+++ test/skeleton_text_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:skeleton_text/skeleton_text.dart';
+
+void main() {
+  test('Adds skeleton text animation to a Widget', () {
+    final skeletonText = SkeletonAnimation(
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: new BorderRadius.circular(30.0),
+            color: Colors.grey[300]),
+        width: 30.0,
+        height: 13.0,
+      ),
+    );
+  });
+}
